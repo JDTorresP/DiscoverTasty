a16c50ef37a8f617279aa72ae3321447424cdf6f
JDTorresP <jd.torres11@uniandes.edu.co>
2017-11-28 12:46:48 -0500

    show Current Position

--- imports/ui/map/Map.jsx
+++ imports/ui/map/Map.jsx
@@ -4,11 +4,45 @@ import Marker from './Marker';
 
 
 class Map extends Component {
+   constructor(props) {
+     super(props);
+     this.state={
+       currentLatitude :0,
+       currentLongitude : 0
+
+
+     };
+     this.getLocation = this.getLocation.bind(this);
+     this.initPosition = this.initPosition.bind(this);
+   }
+    componentWillMount () {
+      this.getLocation();  
+    }
+        
+   
   handleClick(event){
     console.log('lat',event.lat);
     console.log('lng',event.lng);
   }
+  getLocation() {
+    if (navigator.geolocation) {
+        navigator.geolocation.getCurrentPosition(this.initPosition);
+    } else { 
+        alert("Geolocation is not supported by this browser."); 
+    }
+}
+
+initPosition(position) {
+  this.setState({
+    currentLatitude:position.coords.latitude,
+    currentLongitude:position.coords.longitude
+  });
+}
+reGetLocalization(){
+  this.getLocation();
+}
   render() {
+    this.getLocation();
     return (
       <div className="Map">
           <GoogleMapReact
@@ -19,11 +53,13 @@ class Map extends Component {
                   "visibility": "#on"
                 }]
               }]}}
-              center={{lat: 4.699494, lng: -74.055148}}
-              zoom={15}
+              center={this.state.currentLongitude == 0 ? 
+              {lat:4.63239519492501, lng: -74.07368742871091}:
+              {lat:this.state.currentLatitude, lng: this.state.currentLongitude}}
+              zoom={12}
               onClick={(event)=>this.handleClick(event)}
           >
-            <Marker tooltip='Hola' lat='4.7010032' lng='-74.055497'/>
+            <Marker tooltip='Hola' lat={this.state.currentLatitude} lng={this.state.currentLongitude}/>
             
           </GoogleMapReact>
       </div>
