da593124748fc68b733a7cbae89a75b2963e4189
JDTorresP <jd.torres11@uniandes.edu.co>
2017-11-29 19:36:40 -0500

    Done Oauth Facebook & Google

--- imports/ui/Principal.jsx
+++ imports/ui/Principal.jsx
@@ -2,6 +2,10 @@ import React, { Component } from 'react';
 import PropTypes from 'prop-types';
 import AppBar from 'material-ui/AppBar';
 import Map from './map/Map.jsx'
+import List from 'material-ui/List/List';
+import ListItem from 'material-ui/List/ListItem';
+import Avatar from 'material-ui/Avatar';
+import AccountsUIWrapper from './Accounts/AccountsUIWrapper.jsx';
 
 // Principal component - represents Principal component
 class Principal extends Component {
@@ -15,6 +19,17 @@ class Principal extends Component {
                 title="Discover Tasty"
                 iconClassNameRight="muidocs-icon-navigation-expand-more"
             />
+          <AccountsUIWrapper />
+          <List>
+            <ListItem
+                disabled={true}
+                leftAvatar={
+                  <Avatar src="../../client/img/b.png" />
+                }
+                      >
+                Image Avatar
+            </ListItem>
+         </List>
         <Map/>
 
       </div>
--- imports/ui/map/Marker.js
+++ imports/ui/map/Marker.js
@@ -1,11 +1,13 @@
 import React, {Component} from 'react';
 import ReactTooltip from 'react-tooltip';
+import Avatar from 'react-avatar'; 
 
 class Marker extends Component {
   render() {
     return (
         <div data-tip={this.props.tooltip} className="Marker stateBusy">
           <div className="marker">
+            <Avatar googleId="115445798610219700747" className="avatar" round={true} /> 
             <div className="radius"/>
           </div>
           <ReactTooltip place="right" type="dark" effect="float"/>
