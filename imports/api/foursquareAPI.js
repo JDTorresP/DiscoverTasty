4978ba04cc6ef86f53153e2ca32851975a08daef
jsgomez14 <js.gomez14@uniandes.edu.co>
2017-12-06 12:45:33 -0500

    Main UI Modified and foursquareAPI example added

---
+++ imports/api/foursquareAPI.js
@@ -0,0 +1,56 @@
+var foursquare_id = process.env.FOURSQUARE_ID,
+foursquare_secret = process.env.FOURSQUARE_SECRET;
+
+if (! foursquare_secret || ! foursquare_id) {
+console.log('!! You should set both, FOURSQUARE_ID and FOURSQUARE_SECRET env variables');
+}
+
+Meteor.methods({
+    // return array of venues
+    'foursquare-search': function(lat, lng, query, radius) {
+
+    if (! foursquare_secret || ! foursquare_id) {
+    throw new Meteor.Error('Foursquare not configured');
+    }
+
+    if(!this.userId) {
+    throw new Meteor.Error('Permission denied');
+    }
+
+    check(lat, Number);
+    check(lng, Number);
+    check(query, String);
+    check(radius, Number);
+
+    var result,
+        params = {
+        client_id: foursquare_id,
+        client_secret: foursquare_secret,
+        v: 20150606,
+        query: query,
+        radius: radius,
+        limit: 50,
+        ll: '' + lat + ',' + lng,
+    };
+
+    try {
+    result = HTTP.get('https://api.foursquare.com/v2/venues/search', {
+        params: params,
+        timeout: 20000
+    });
+    } catch(error) {
+    throw new Meteor.Error('Foursquare api call failed');
+    }
+
+    // save query in db
+    Queries.insert({
+    userId: this.userId,
+    lat: lat,
+    lng: lng,
+    query: query,
+    radius: radius,
+    });
+
+    return result.data.response.venues;
+    },
+});
--- imports/ui/Principal.jsx
+++ imports/ui/Principal.jsx
@@ -6,32 +6,67 @@ import List from 'material-ui/List/List';
 import ListItem from 'material-ui/List/ListItem';
 import Avatar from 'material-ui/Avatar';
 import AccountsUIWrapper from './Accounts/AccountsUIWrapper.jsx';
+import {Tabs, Tab} from 'material-ui/Tabs';
+
+const styles = {
+  headline: {
+    fontSize: 24,
+    paddingTop: 16,
+    marginBottom: 12,
+    fontWeight: 400,
+  },
+};
 
 // Principal component - represents Principal component
 class Principal extends Component {
     constructor(props) {
         super(props);
+        this.state = {
+          value: 'a',
+        };
     }
+
+  handleChange = (value) => {
+    this.setState({
+      value: value,
+    });
+  };
+  
   render() {
     return (
       <div className="Principal"> 
-        <AppBar
-                title="Discover Tasty"
-                iconClassNameRight="muidocs-icon-navigation-expand-more"
-            />
-          <AccountsUIWrapper />
-          <List>
-            <ListItem
-                disabled={true}
-                leftAvatar={
-                  <Avatar src="../../client/img/b.png" />
-                }
-                      >
-                Image Avatar
-            </ListItem>
-         </List>
+        <AccountsUIWrapper />
+        <Tabs
+          value={this.state.value}
+          onChange={this.handleChange.bind(this)}
+        >
+          <Tab label="Restaurants" value="a">
+            <div>
+              <h2 style={styles.headline}>Controllable Tab A</h2>
+              <p>
+                Here's where a list of restaurants will show up.
+              </p>
+            </div>
+          </Tab>
+          <Tab label="Itineraries" value="b">
+            <div>
+              <h2 style={styles.headline}>Controllable Tab B</h2>
+              <p>
+                here's where a list of Itineraries will show up
+              </p>
+            </div>
+          </Tab>
+        </Tabs>
+        <List>
+        <ListItem
+          disabled={true}
+          leftAvatar={
+          <Avatar src="../../client/img/b.png" />
+          }>
+          Image Avatar
+        </ListItem>
+        </List>
         <Map/>
-
       </div>
     );
   }
